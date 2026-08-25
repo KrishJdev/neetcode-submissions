b01d12daa80a699d9df4c6f75c38db4c808c6525
Krish Jaiswal <88109912+KrishJdev@users.noreply.github.com>
2026-08-25 19:22:54 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if(stack.isEmpty())
+                return false;
+                char top = stack.peek();
+                if (top == '(' && c == ')') {
+                    stack.pop();
+                } else if (top == '[' && c == ']') {
+                    stack.pop();
+                } else if (top == '{' && c == '}') {
+                    stack.pop();
+                } else
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
